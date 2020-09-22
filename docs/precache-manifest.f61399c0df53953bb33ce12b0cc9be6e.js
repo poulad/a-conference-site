fb98096b37f42f6664356f735b6c1f3d10d3fe82
Poulad <poulad.pld@gmail.com>
2020-09-22 07:25:13 -0400

    Build for gh pages

---
+++ docs/precache-manifest.f61399c0df53953bb33ce12b0cc9be6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1382a49f507edd0f554218533f7ad44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02c7c68caef92b178c20",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bab144680513d9d09340",
+    "url": "/static/js/2.38977539.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.38977539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02c7c68caef92b178c20",
+    "url": "/static/js/main.a0184605.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
