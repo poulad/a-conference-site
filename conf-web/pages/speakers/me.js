8017fd454a75b991555c9e0530d5738cb4c4d437
Poulad <poulad.pld@gmail.com>
2020-09-26 13:31:53 -0400

    Enhance styles

---
+++ conf-web/pages/speakers/me.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Head from 'next/head'
+import Layout from "../../components/layout";
+
+
+export default function () {
+  return <Layout>
+    <Head>
+      <title>Speaker - !</title>
+    </Head>
+    <h1>Hello, world</h1>
+  </Layout>
+}
