a78e595b60160eb85f24cc9a4e4bd8c1f93e8b57
Poulad <poulad.pld@gmail.com>
2020-10-21 07:40:00 -0400

    Save theme preference in local storage

--- conf-web/providers/use-theme.tsx
+++ conf-web/providers/use-theme.tsx
@@ -7,15 +7,61 @@ const ThemeContext = React.createContext(
   }
 );
 
-function getInitialTheme(): string {
-  let initialTheme = "light";
-  // TODO read from local storage
+const AVAILABLE_THEMES = ["light", "dark"];
+const DEFAULT_THEME = AVAILABLE_THEMES[0];
+const LOCAL_STORAGE_KEY_THEME = "theme";
 
-  return initialTheme;
+let initialThemeSource = "DEFAULT" as
+  | "DEFAULT"
+  | "LOCAL_STORAGE"
+  | "MEDIA_QUERY";
+
+function getInitialTheme(): string {
+  if (typeof localStorage === "undefined") return DEFAULT_THEME;
+
+  let initTheme = DEFAULT_THEME;
+  try {
+    const themeInLocalStorage = localStorage.getItem(LOCAL_STORAGE_KEY_THEME);
+    if (AVAILABLE_THEMES.includes(themeInLocalStorage)) {
+      initTheme = themeInLocalStorage;
+      initialThemeSource = "LOCAL_STORAGE";
+    } else {
+      console.warn(
+        `Invalid theme value "${themeInLocalStorage}" in the local storage will be cleared.`
+      );
+      localStorage.removeItem(LOCAL_STORAGE_KEY_THEME);
+    }
+  } catch (e) {
+    console.warn(
+      `Failed to find the initial theme value in local storage. Using default value "${initTheme}".`,
+      e
+    );
+  }
+
+  return initTheme;
 }
 
 export function ThemeProvider(props) {
-  const [theme, setTheme] = useState(getInitialTheme);
+  const [theme, _setThemeState] = useState(getInitialTheme);
+
+  const setTheme = (value) => {
+    if (theme === value) return;
+
+    if (!AVAILABLE_THEMES.includes(value)) {
+      console.error(`Theme "${value}" is invalid!`);
+      return;
+    }
+
+    _setThemeState(value);
+
+    if (typeof localStorage === "undefined") return;
+
+    try {
+      localStorage.setItem(LOCAL_STORAGE_KEY_THEME, value);
+    } catch (e) {
+      console.warn(`Failed to set the theme value in local storage.`, e);
+    }
+  };
 
   useEffect(() => {
     if (typeof window.matchMedia !== "function") return;
@@ -29,7 +75,10 @@ export function ThemeProvider(props) {
     };
 
     const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-    updateThemeForUserPreference(prefersDarkScheme.matches);
+    if (initialThemeSource === "DEFAULT") {
+      updateThemeForUserPreference(prefersDarkScheme.matches);
+      initialThemeSource = "MEDIA_QUERY";
+    }
 
     const colorSchemeQueryListener = (ev: MediaQueryListEvent) => {
       updateThemeForUserPreference(ev.matches);
